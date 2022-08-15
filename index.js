09493b559c98fab556215b7c57afcbf1ce037edd
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-15 18:58:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+
+
+const currentUser = 'Mercy Milkah';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+const excitedWelcomeMessage = `(${welcomeMessage}).toUpperCase()`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+// const shortGreeting = `Welcome,  ${currentUser[0]}!`;
+
+const firtLetter = currentUser[0];
+const otherLetters = currentUser.slice(1);
+const Greeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
